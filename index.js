51f06d29f6e206593c889e1644b78ba4deb359c8
suyash-iitj <154698199+suyash-iitj@users.noreply.github.com>
2025-03-28 13:08:04 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import ClauseCheckApp from "./ClauseCheckApp";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ClauseCheckApp />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
